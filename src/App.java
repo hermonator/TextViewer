01656d870ba582bda249a5aa370ce40b4852f3bb
Jesse Hermon <hermonator3@gmail.com>
2015-06-05 15:55:14 +1000

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jesse_000 on 6/5/2015.
+ */
+public class App {
+    public static void main(String[] args) {
+        String text = "This is a simple test\nOne Two Three\nHello world!";
+        for (int i = 0; i < text.length(); ++i) {
+            System.out.print(text.charAt(i));
+        }
+        System.out.println();
+    }
+}
